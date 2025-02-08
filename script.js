61a64955936a1d8dcd3e64f0e26a2873e04fa2ca
sonali173 <sonalisinghsoomo@gmail.com>
2025-02-08 17:12:26 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+const typed = new Typed('.multiple-text', {
+  strings: ['web development', 'SEO', 'HTML', 'CSS', 'JavaScript', 'Testing'],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 1200,
+  loop: true,
+});
+
+const menuT = document.querySelector('#menu-icon');
+const navLink = document.querySelector('.navbar');
+
+menuT.addEventListener('click', () => {
+
+  menuT.classList.toggle('active');
+  navLink.classList.toggle('active');
+});
+
+
